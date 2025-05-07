4eac26c6dd9640e50d9a237f225bb383cb1746ef
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-05-07 14:33:16 -0500

    Added the delete functionality on GameList component

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Game-Manager-App
 Built a full stack app using React + Express by following a React course tutorial. Included zustand, React Query, React Hook Form and React Query.
+*NOTE: I added the invalidating queries and handleDelete functions in the GameList component by myself.
--- client/src/components/GameList.jsx
+++ client/src/components/GameList.jsx
@@ -1,7 +1,31 @@
 import { Link } from "react-router-dom";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+
+async function deleteGame(id) {
+  const response = await fetch(`http://localhost:3000/api/games/${id}`, {
+    method: "DELETE",
+  });
+  return response.json();
+}
 
 const GameList = (props) => {
   const games = props.games;
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  
+  const {mutate} = useMutation({mutationFn: deleteGame, onSuccess: ()=> {
+    queryClient.invalidateQueries(["games"]);
+    navigate("/")
+  }})
+
+  const handleDelete = (id) => {
+    if(window.confirm("Are you sure you want to delete this game?")){
+      mutate(id);
+    }
+  }
+
+
   return (
     <div className="games-grid">
       {games.map((game) => (
@@ -10,6 +34,7 @@ const GameList = (props) => {
           <p>{game.platform}</p>
           <p>{game.genre}</p>
           <div><Link to={`/update/${game.id}`}>Edit</Link></div>
+          <button onClick={()=> handleDelete(game.id)}>Delete</button>
         </div>
       ))}
     </div>
