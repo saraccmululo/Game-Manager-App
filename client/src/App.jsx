df9c5f8e24ba1548c0fa86805082146a9381c226
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-05-23 15:19:46 -0500

    Fixed import typo on App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import CreateGame from './pages/CreateGame'
 import UpdateGame from './pages/UpdateGame'
 import NotFound from './pages/NotFound'
